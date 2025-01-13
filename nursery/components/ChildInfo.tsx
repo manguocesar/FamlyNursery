2f62067fc324f2ed4087d718d21fb81fae616542
Cesar <cesar.hertz@icloud.com>
2025-01-13 22:40:14 +0100

    library/use date-fns

--- nursery/components/ChildInfo.tsx
+++ nursery/components/ChildInfo.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import styles from "./child.module.css";
 import { ChildrenInfo } from '../types/types'
+import { useFormatter } from 'next-intl';
+import {formatDistance, format} from 'date-fns';
 
 
-export default function ChildInfo(child:ChildrenInfo) {
+export default function ChildInfo(child: ChildrenInfo) {
 
-  let date = new Date(child.child.checkinTime);
+  let date = format(child.child.checkinTime, "eeee dd LLL")
+  let time = format(child.child.checkinTime, "HH:mm")
 
   return (
     <div className={styles.childInfo}>
-            <h2>Child Info:</h2>
-            <div>Gender: {child.child.gender === 1 ? "Boy"  : "Girl"}</div>
-            <div style={{textAlign: 'center'}}>Checking date: {date.toLocaleDateString()}</div>
-            <div style={{textAlign: 'center'}}>Checking time: {date.toLocaleTimeString()}</div>
-          
-          </div>
+      <h2>Child Info:</h2>
+      <div>Gender: {child.child.gender === 1 ? "Boy" : "Girl"}</div>
+      <div style={{ textAlign: 'center' }}>Checking date: {date}</div>
+      <div style={{ textAlign: 'center' }}>Checking time: {time}</div>
+    </div>
   )
 }
