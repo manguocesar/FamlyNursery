59de8b1c861a3af4e704f56d5846e54dfdaf2b4e
Cesar <cesar.hertz@icloud.com>
2025-01-13 21:36:20 +0100

    check child btn

--- nursery/components/CheckChild.tsx
+++ nursery/components/CheckChild.tsx
@@ -1,8 +1,30 @@
 import { ChildrenInfo } from '@/types/types'
 import React from 'react'
+import styles from "./child.module.css";
 
-export default function CheckChild(child:ChildrenInfo){
+export default function CheckChild(child: ChildrenInfo) {
+
+    const CheckChild = async () => {
+
+      console.log("hey");
+      
+
+      // fetch('https://app.famly.co/api/v2/children/<childId>/checkins', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      //   body: JSON.stringify(child.child),
+      // })
+
+
+    }
+
+
   return (
-    <div>CheckChild</div>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      <div className={styles.checkChild} onClick={()=> CheckChild}>Check Child</div>
+      <div>{child.child.childId}</div>
+    </div>
   )
 }
