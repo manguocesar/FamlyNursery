5bea0e4e35d9f75d987b272480da7b734f49ef3b
Cesar <cesar.hertz@icloud.com>
2025-01-13 21:53:39 +0100

    remove unused next js config

--- nursery/next.config.ts
+++ nursery/next.config.ts
@@ -1,17 +1,5 @@
 import type { NextConfig } from "next";
-
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.example.com',
-        port: '',
-        pathname: '/account123/**',
-        search: '',
-      },
-    ],
-  },
-}
-
+  /* config options here */
+};
 export default nextConfig;
