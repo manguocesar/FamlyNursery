ad1e98d16bf23d3d0dd841eaed7773f5124e7672
Cesar <cesar.hertz@icloud.com>
2025-01-15 18:24:44 +0100

    dynamic checkInOut children react query

--- nursery/components/ChildCard.tsx
+++ nursery/components/ChildCard.tsx
@@ -1,53 +1,144 @@
 import { checkInChild, checkOutChild } from '@/actions/children';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import ChildIntro from './ChildIntro';
 import ChildInfo from './ChildInfo';
 import { Child as ChildType } from '@/types/children';
+import { useMutation, useQuery,QueryClient, useQueryClient } from '@tanstack/react-query';
+import { useInView } from 'react-intersection-observer';
 
-export default function Child({ child }: { child: ChildType }) {
+const fetchChildren = async () => {
+  try {
+    const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+    const groupId = '86413ecf-01a1-44da-ba73-1aeda212a196';
+    const institutionId = 'dc4bd858-9e9c-4df7-9386-0d91e42280eb';
+    const URL = `https://app.famly.co/api/daycare/tablet/group?accessToken=${accessToken}&groupId=${groupId}&institutionId=${institutionId}`;
+
+    const res = await fetch(URL);
+    const { children } = await res.json();
+    return children;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default function Child() {
   const now = format(Date(), 'HH:mm');
   const [time, setTime] = useState<string>(now);
+  const { ref, inView } = useInView();
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['children'],
+    queryFn: fetchChildren,
+    staleTime: 10000,
+  });
+
+  const mutateCheckOut = useMutation({
+    mutationFn: checkOutChild,
+    onSuccess: () => {
+        queryClient.invalidateQueries(["children"]);
+    },
+    onMutate: async (childId) => {
+        await queryClient.cancelQueries(["children"]);
+        const previousPosts = queryClient.getQueryData(["children"]);
+        queryClient.setQueryData(["children"], (childId:number) => childId);
+
+        return { previousPosts };
+    },
+    onError: (err, newPost, context) => {
+        queryClient.setQueryData(["children"], context?.previousPosts);
+    },
+});
+  
+const mutateCheckIn = useMutation({
+    mutationFn: checkInChild,
+    onSuccess: (childId, time) => {
+        queryClient.invalidateQueries(["children"]);
+    },
+    onMutate: async (childId, time) => {
+        await queryClient.cancelQueries(["children"]);
+        const previousPosts = queryClient.getQueryData(["posts"]);
+        queryClient.setQueryData(["children"], (
+          childId:number, time:string) => childId, time);
+
+        return { previousPosts };
+    },
+    onError: (err, newPost, context) => {
+        queryClient.setQueryData(["children"], context?.previousPosts);
+    },
+});
+
+  const [displayChildren, setDisplayChildren] = useState<number>(5);
+
+  useEffect(() => {
+    if (inView) {
+      setDisplayChildren(displayChildren => displayChildren + 3);
+    }
+  }, [inView]);
+
+  const slicedArray = data?.slice(0, displayChildren);
+
+  console.log('data', data);
+
+  if (isLoading) return <p> Loading...</p>;
+
+  if (error) return <p> Error occured: {error.message}</p>;
 
   return (
-    <div
-      key={child.childId}
-      className="m-3 flex justify-around rounded-lg border-2 p-3"
-    >
-      <ChildIntro child={child} />
-      <ChildInfo child={child} />
-
-      <div className="">
-        {child.checkedIn ? (
-          <button
-            className="m-3 rounded-lg border-b-0 border-r-2 bg-white p-3"
-            onClick={() => {
-              checkOutChild(child.childId);
-            }}
-          >
-            Check Out Child now at {time}
-          </button>
-        ) : (
-          <div className="flex flex-col items-center">
-            <button
-              className="m-3 rounded-lg border-b-0 border-r-2 bg-white p-3"
-              onClick={() => {
-                checkInChild(child.childId, time);
-              }}
-            >
-              Check In Child at
-            </button>
-            <input
-              value={time}
-              onChange={ev => {
-                setTime(ev.target.value);
-              }}
-              aria-label="Time"
-              type="time"
-            />
+    <div className="flex flex-col bg-gray-400">
+      <h1 className="text-center text-5xl font-extrabold">Nursery</h1>
+      <div className="grid grid-rows-3 gap-4 p-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
+        {slicedArray?.map((child: ChildType) => (
+          <div key={child.childId} className="flex rounded-md bg-white p-4">
+            <ChildIntro child={child} />
+            <ChildInfo child={child} />
+            {child.checkedIn ? (
+              <button
+                className="m-3 rounded-lg border-b-0 border-r-2 bg-white p-3"
+                onClick={() => {
+                  mutateCheckOut.mutate(child.childId);
+                }}
+              >
+                Check Out Child now at {time}
+              </button>
+            ) : (
+              <div className="flex flex-col items-center">
+                <button
+                  className="m-3 rounded-lg border-b-0 border-r-2 bg-white p-3"
+                  onClick={() => {
+                    mutateCheckIn.mutate(child.childId, time)
+                  }}
+                >
+                  Check In Child at
+                </button>
+                <input
+                  value={time}
+                  onChange={ev => {
+                    setTime(ev.target.value);
+                  }}
+                  aria-label="Time"
+                  type="time"
+                />
+              </div>
+            )}
           </div>
-        )}
+        ))}
+
+        <div className="flex justify-center p-4">
+          <button
+            onClick={() =>
+              setDisplayChildren(displayChildren => displayChildren + 3)
+            }
+            className="rounded-md bg-blue-500 px-4 py-2 text-3xl font-bold text-white hover:bg-blue-700"
+          >
+            Display more children
+          </button>
+        </div>
       </div>
+      <p className="text-center" ref={ref}>
+        Loading more children...
+      </p>
     </div>
   );
 }
--- nursery/pages/index.tsx
+++ nursery/pages/index.tsx
@@ -7,86 +7,15 @@ import {
   QueryClientProvider,
   useQuery,
 } from '@tanstack/react-query';
+
 import ChildCard from '@/components/ChildCard';
 
-const fetchChildren = async () => {
-  try {
-    const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-    const groupId = '86413ecf-01a1-44da-ba73-1aeda212a196';
-    const institutionId = 'dc4bd858-9e9c-4df7-9386-0d91e42280eb';
-    const URL = `https://app.famly.co/api/daycare/tablet/group?accessToken=${accessToken}&groupId=${groupId}&institutionId=${institutionId}`;
-
-    const res = await fetch(URL);
-    const { children } = await res.json();
-    return children;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
 export default function Home() {
-  const [children, setChildren] = useState<Child[]>([]);
-  const { ref, inView } = useInView();
-
-  useEffect(() => {
-    const fetchChildren = async () => {
-      try {
-        const accessToken = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-        const groupId = '86413ecf-01a1-44da-ba73-1aeda212a196';
-        const institutionId = 'dc4bd858-9e9c-4df7-9386-0d91e42280eb';
-        const URL = `https://app.famly.co/api/daycare/tablet/group?accessToken=${accessToken}&groupId=${groupId}&institutionId=${institutionId}`;
-
-        const res = await fetch(URL);
-        const { children } = await res.json();
-        setChildren(children);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchChildren();
-  }, []);
-
-  useEffect(() => {
-    if (inView) {
-      setDisplayChildren(displayChildren => displayChildren + 3);
-    }
-  }, [inView]);
-
-  const [displayChildren, setDisplayChildren] = useState<number>(5);
-
-  const slicedArray = children?.slice(0, displayChildren);
-
-  // const { data, isLoading, error } = useQuery({
-  //   queryKey: ['posts'],
-  //   queryFn: fetchChildren,
-  //   staleTime: 10000,
-  // });
-
-  // console.log('data', data);
-
-  // if (isLoading) return <p> Loading...</p>;
-
-  // if (error) return <p> Error occured: {error.message}</p>;
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <div className="flex flex-col bg-gray-400">
-      <h1 className="text-center text-5xl font-extrabold">Nursery</h1>
-      <div className="grid grid-rows-3 gap-4 p-2 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3">
-        {slicedArray?.map((child: Child) => <ChildCard child={child} />)}
-      </div>
-      <div className="flex justify-center p-4">
-        <button
-          onClick={() =>
-            setDisplayChildren(displayChildren => displayChildren + 3)
-          }
-          className="rounded-md bg-blue-500 px-4 py-2 text-3xl font-bold text-white hover:bg-blue-700"
-        >
-          Display more children
-        </button>
-      </div>
-      <p className="text-center" ref={ref}>
-        Loading more children...
-      </p>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ChildCard />
+    </QueryClientProvider>
   );
 }
